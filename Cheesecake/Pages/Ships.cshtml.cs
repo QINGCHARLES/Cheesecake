cebefe6b27834ccdfc574787e936adf5687e2cfe
QINGCHARLES <kingcharlesother@gmail.com>
2023-07-24 14:24:51 -0500

    24jul23a

---
+++ Cheesecake/Pages/Ships.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cheesecake.Pages
+{
+    public class ShipsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
