212b1da159f75fa7653a7afe60b2938c932cccdf
QINGCHARLES <kingcharlesother@gmail.com>
2023-08-01 00:15:29 -0500

    1aug23am1

--- Cheesecake/wwwroot/js/site.js
+++ Cheesecake/wwwroot/js/site.js
@@ -53,6 +53,193 @@ document.addEventListener('click', function (event)
 	});
 });
 
+const TWO_PI = Math.PI * 2;
+const HALF_PI = Math.PI * 0.5;
+
+// canvas settings
+var viewWidth = 512,
+	viewHeight = 550,
+	drawingCanvas = document.getElementById("drawing_canvas"),
+	ctx,
+	timeStep = (1 / 60);
+
+Point = function (x, y)
+{
+	this.x = x || 0;
+	this.y = y || 0;
+};
+
+Particle = function (p0, p1, p2, p3)
+{
+	this.p0 = p0;
+	this.p1 = p1;
+	this.p2 = p2;
+	this.p3 = p3;
+
+	this.time = 0;
+	this.duration = 3 + Math.random() * 2;
+	this.color = '#' + Math.floor((Math.random() * 0xffffff)).toString(16);
+
+	this.w = 8;
+	this.h = 6;
+
+	this.complete = false;
+};
+
+Particle.prototype = {
+	update: function ()
+	{
+		this.time = Math.min(this.duration, this.time + timeStep);
+
+		var f = Ease.outCubic(this.time, 0, 1, this.duration);
+		var p = cubeBezier(this.p0, this.p1, this.p2, this.p3, f);
+
+		var dx = p.x - this.x;
+		var dy = p.y - this.y;
+
+		this.r = Math.atan2(dy, dx) + HALF_PI;
+		this.sy = Math.sin(Math.PI * f * 10);
+		this.x = p.x;
+		this.y = p.y;
+
+		this.complete = this.time === (this.duration);
+	},
+	draw: function ()
+	{
+		ctx.save();
+		ctx.translate(this.x, this.y);
+		ctx.rotate(this.r);
+		ctx.scale(1, this.sy);
+
+		ctx.fillStyle = this.color;
+		ctx.fillRect(-this.w * 3.5, -this.h * 0.5, this.w, this.h);
+
+		ctx.restore();
+	}
+};
+
+var particles = [];
+
+function initDrawingCanvas()
+{
+	drawingCanvas.width = viewWidth;
+	drawingCanvas.height = viewHeight;
+	ctx = drawingCanvas.getContext('2d');
+	createParticles();
+}
+
+function createParticles()
+{
+	for (var i = 0; i < 128; i++)
+	{
+		var p0 = new Point(viewWidth * 0.5, viewHeight * 0.5);
+		var p1 = new Point(Math.random() * viewWidth, Math.random() * viewHeight);
+		var p2 = new Point(Math.random() * viewWidth, Math.random() * viewHeight);
+		var p3 = new Point(Math.random() * viewWidth, viewHeight + 64);
+
+		particles.push(new Particle(p0, p1, p2, p3));
+	}
+}
+
+function update()
+{
+
+	particles.forEach(function (p)
+	{
+		p.update();
+	});
+}
+
+function draw()
+{
+	ctx.clearRect(0, 0, viewWidth, viewHeight);
+
+
+	particles.forEach(function (p)
+	{
+		p.draw();
+	});
+}
+
+function shootConfetti()
+{
+	particles = [];
+	initDrawingCanvas();
+
+	requestAnimationFrame(loop);
+};
+
+function loop()
+{
+	update();
+	draw();
+
+
+	if (checkParticlesComplete())
+	{
+		particles = [];
+		particles.length = 0;
+		//createParticles();
+		return;
+	}
+
+	requestAnimationFrame(loop);
+}
+
+function checkParticlesComplete()
+{
+	for (var i = 0; i < particles.length; i++)
+	{
+		if (particles[i].complete === false) return false;
+	}
+	return true;
+}
+
+// math and stuff
+
+/**
+ * easing equations from http://gizma.com/easing/
+ * t = current time
+ * b = start value
+ * c = delta value
+ * d = duration
+ */
+var Ease = {
+	inCubic: function (t, b, c, d)
+	{
+		t /= d;
+		return c * t * t * t + b;
+	},
+	outCubic: function (t, b, c, d)
+	{
+		t /= d;
+		t--;
+		return c * (t * t * t + 1) + b;
+	},
+	inOutCubic: function (t, b, c, d)
+	{
+		t /= d / 2;
+		if (t < 1) return c / 2 * t * t * t + b;
+		t -= 2;
+		return c / 2 * (t * t * t + 2) + b;
+	},
+	inBack: function (t, b, c, d, s)
+	{
+		s = s || 1.70158;
+		return c * (t /= d) * t * ((s + 1) * t - s) + b;
+	}
+};
+
+function cubeBezier(p0, c0, c1, p1, t)
+{
+	var p = new Point();
+	var nt = (1 - t);
+
+	p.x = nt * nt * nt * p0.x + 3 * nt * nt * t * c0.x + 3 * nt * t * t * c1.x + t * t * t * p1.x;
+	p.y = nt * nt * nt * p0.y + 3 * nt * nt * t * c0.y + 3 * nt * t * t * c1.y + t * t * t * p1.y;
+
+	return p;
+}
 
 function delayOneSecond()
 {
@@ -78,9 +265,9 @@ var model = {
 
 	fire: function (guess)
 	{
-		if (guess === this.chest) { view.displayChest(guess); return; }
-		if (guess === this.mermaid) { view.displayMermaid(guess); return; }
-		if (guess === this.monster) { view.displayMonster(guess); return; }
+		if (guess === this.chest) { view.displayChest(guess); shootConfetti(); return; }
+		if (guess === this.mermaid) { view.displayMermaid(guess); shootConfetti(); return; }
+		if (guess === this.monster) { view.displayMonster(guess); shootConfetti(); return; }
 
 		for (var i = 0; i < this.numShips; i++)
 		{
@@ -100,6 +287,7 @@ var model = {
 					view.displayMessage("You sunk my battleship!");
 					view.displaySunk(ship);
 					this.shipsSunk++;
+					shootConfetti();
 				}
 				return true;
 			}
